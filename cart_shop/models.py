42be0e7ba78ebc011010a3f5d299add41de38325
katasrofi <danielsensei42@gmail.com>
2025-01-31 21:28:24 +0700

    add

--- cart_shop/models.py
+++ cart_shop/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class CartShop(models.Model):
+<<<<<<< Updated upstream
     product = models.CharField(max_length=200) 
+=======
+    product 
+>>>>>>> Stashed changes
