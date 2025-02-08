f13a03d102187aa328d4ef5c933403971904b458
katasrofi <danielsensei42@gmail.com>
2025-02-08 09:33:50 +0700

    create CartShop models

--- cart_shop/models.py
+++ cart_shop/models.py
@@ -1,7 +1,21 @@
+# Django Library 
 from django.db import models
 
 # Create your models here.
 
 class CartShop(models.Model):
-<<<<<<< Updated upstream
-    product = models.CharField(max_length=200) 
+    # Relation
+    user_id = models.ForeignKey('user_shop.CustomUser',
+                                related_name='carts',
+                                on_delete=models.CASCADE)
+    product_id = models.ForeignKey('product_shop.Product',
+                                   related_name='cart_items',
+                                   on_delete=models.CASCADE) 
+
+    # Total of product in cart 
+    quantity = models.PositiveIntegerField(default=1)
+
+    # Calculate the total price 
+    @property 
+    def total_price(self):
+        return self.product_id.price * self.quantity 
