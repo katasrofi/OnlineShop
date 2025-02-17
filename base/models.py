730a57d0d023d37031f767d5b0babc5cb0e16f08
katasrofi <danielsensei42@gmail.com>
2025-02-17 16:56:10 +0700

    move wishlish from user_shop into base for avoid circular import

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Wishlist(models.Model):
+    # Composite key
+    user_id = models.ForeignKey(
+        "user_shop.CustomUser", related_name="user_wishlists", on_delete=models.CASCADE
+    )
+    product_id = models.ForeignKey(
+        "product_shop.Product",
+        related_name="product_wishlists",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
