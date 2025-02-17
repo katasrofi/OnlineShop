d2419dc48a2bd4433f272dad55c3121b8fae9be9
katasrofi <danielsensei42@gmail.com>
2025-02-17 16:56:17 +0700

    add home urls

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
-# Base Urls 
+# Base Urls
 from django.urls import path
-from . import views 
+from . import views
+
+app_name = "base"
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'), 
+    path("", views.Home.as_view(), name="home"),
 ]
