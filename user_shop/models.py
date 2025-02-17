e9e219b22c156a6e53882c28573d2088bbfff7b1
katasrofi <danielsensei42@gmail.com>
2025-02-17 17:00:07 +0700

    organize the models

--- user_shop/models.py
+++ user_shop/models.py
@@ -1,89 +1,76 @@
 # Django Library
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Model from different apps
-from product_shop.models import Product
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, 
-                    email, 
-                    password=None,
-                    **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Email need to fill')
+            raise ValueError("Email need to fill")
 
         email = self.normalize_email(email)
-        user = self.model(email=email,
-                          **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        return user 
+        return user
 
-    def create_superuser(self,
-                         email,
-                         password=None,
-                         **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
+        # Set superuser authority
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-        return self.create_user(email,
-                                password, 
-                                **extra_fields)
 
-class CustomUser(AbstractBaseUser, 
-                 PermissionsMixin):
+        # Validation the superuser authority
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     # Users information
     name = models.CharField(max_length=500)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=30,
-                                    blank=True, 
-                                    null=True)
+    phone_number = models.CharField(max_length=30, blank=True, null=True)
 
     # Role Choice
     role_choice = [
-            ('ADMIN', 'Admin'),
-            ('CUSTOMER', 'CUSTOMER'),
-            ('SELLER', 'Seller'),
-            ]
-    role = models.CharField(max_length=20,
-                            choices=role_choice,
-                            default='CUSTOMER')
+        ("ADMIN", "Admin"),
+        ("CUSTOMER", "CUSTOMER"),
+        ("SELLER", "Seller"),
+    ]
+    role = models.CharField(max_length=20, choices=role_choice, default="CUSTOMER")
 
     # Registration
     created_at = models.DateTimeField(auto_now_add=True)
 
-    # Status and permissions 
+    # Status and permissions
     is_active = models.BooleanField(default=True)
-    is_staf = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
+
     REQUIRED_FIELDS = ["name"]
 
     def __str__(self):
-        return self.email 
+        return self.email
 
-class Wishlist(models.Model):
-    # Composite key 
-    user_id = models.ForeignKey(CustomUser,
-                                related_name='user_wishlists',
-                                on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product,
-                                   related_name='product_wishlists',
-                                   on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 class Notification(models.Model):
     # Relation
-    user_id = models.ForeignKey(CustomUser,
-                                related_name='user_notif',
-                                on_delete=models.CASCADE)
-    
-    # Notification information 
+    user_id = models.ForeignKey(
+        CustomUser, related_name="user_notif", on_delete=models.CASCADE
+    )
+
+    # Notification information
     title = models.CharField(max_length=100)
     content = models.TextField()
     status = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
-
