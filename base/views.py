d03750b2a17efc79c4765835af9f76aac6acdd2b
katasrofi <danielsensei42@gmail.com>
2025-02-17 16:56:24 +0700

    add home views

--- base/views.py
+++ base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views import generic 
+from django.views import generic
+from product_shop.models import Product
+
 
 # Create your views here.
 class Home(generic.TemplateView):
-    template_name = 'base/home.html' 
+    template_name = "base/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_list"] = Product.objects.all()
+        return context
