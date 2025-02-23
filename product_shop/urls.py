59cc13348ea2d2d72f2371b86cb5d3773650a64c
katasrofi <danielsensei42@gmail.com>
2025-02-23 15:16:42 +0700

    add product_detail url and search product url

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 app_name = "product_shop"
 
-urlpatterns = []
+urlpatterns = [ 
+    path('product_detail/<str:pk>/', views.ProductDetail.as_view(), name='product_detail'),
+    path('search_product/', views.search_product, name='search_product'),
+]
