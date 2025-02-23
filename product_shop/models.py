7a516d6ae0ab37640ff1d294bd330052c2f95088
katasrofi <danielsensei42@gmail.com>
2025-02-23 15:16:23 +0700

    change category_name to name in Category models

--- product_shop/models.py
+++ product_shop/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
-        return self.category_name
+        return self.name
 
 
 class Product(models.Model):
