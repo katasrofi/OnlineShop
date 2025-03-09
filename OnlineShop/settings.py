24db6d43244ccd65686bcb19e1fdf00630fd01eb
katasrofi <danielsensei42@gmail.com>
2025-03-09 10:38:24 +0700

    Change DB from sqlite into postgres

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -115,8 +115,14 @@ WSGI_APPLICATION = "OnlineShop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "USER": os.getenv("DB_USER"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
