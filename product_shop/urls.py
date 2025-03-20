3a96688d5e6d943ddad5cab9f55bba51ce4ed08b
katasrofi <danielsensei42@gmail.com>
2025-03-21 06:31:00 +0700

    add add_product url

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -7,4 +7,5 @@ app_name = "product_shop"
 urlpatterns = [ 
     path('product_detail/<str:pk>/', views.ProductDetail.as_view(), name='product_detail'),
     path('search_product/', views.search_product, name='search_product'),
+    path('add_product/', views.add_product, name='add_product')
 ]
