59e53581235985a363bae97486500dd1aef22167
katasrofi <danielsensei42@gmail.com>
2025-03-21 06:32:20 +0700

    add refresh token and get token into url

--- user_shop/urls.py
+++ user_shop/urls.py
@@ -1,4 +1,5 @@
 # User Urls
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path("register/", views.RegisterPage.as_view(), name="register"),
     path("login/", views.LoginPage.as_view(), name="login"),
     path("logout/", views.LogoutPage.as_view(), name="logout"),
+    path("api/token/", views.EmailTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/refresh/", TokenRefreshView.as_view(), name="token_refresh"), 
 ]
