cbad0b625534f70b1aea43afb43a1a377477cb86
katasrofi <danielsensei42@gmail.com>
2025-03-21 06:29:26 +0700

    web interface setup, still in init stage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    autoprefixer(),
   ],
-  build: {
-    outDir: "static",
+  build:{
+    outDir: "../static/css",
+    emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: "css/styles.css"
+        entryFileNames: "app.js",
+        assetFileNames: "styles.css",
       },
     },
   },
