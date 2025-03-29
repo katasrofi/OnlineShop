5b401a8140e5083d52216cc874b1ed99abe2c816
katasrofi <danielsensei42@gmail.com>
2025-03-29 13:42:11 +0700

    change product detail from CBV to FBV

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "product_shop"
 
 urlpatterns = [ 
-    path('product_detail/<str:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-    path('search_product/', views.search_product, name='search_product'),
-    path('add_product/', views.add_product, name='add_product')
+    path("product_detail/<str:pk>", views.product_detail, name="product_detail"), 
+    # path("product_detail/<str:pk>/", views.ProductDetail.as_view(), name="product_detail"),
+    path("search_product/", views.search_product, name="search_product"),
+    path("add_product/", views.add_product, name="add_product")
 ]
