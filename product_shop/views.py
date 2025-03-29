7ec9e8798374aa010a42d7aac4a4dc20171c070e
katasrofi <danielsensei42@gmail.com>
2025-03-29 13:42:26 +0700

    cleanup the view

--- product_shop/views.py
+++ product_shop/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import (
+        render,
+        get_object_or_404,
+)
 from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -8,31 +11,38 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
 # Models 
-from .models import Product, Category
+from .models import (
+        Product, 
+        Category,
+)        
 
+# Forms 
+from .forms import (
+        AddProductForm,
+)
 # Create your views here.
 def product_detail(request, pk):
-    product_data = Product.objects.all()
+    product_data = get_object_or_404(Product, pk=pk)
     template_name = "product_shop/product_detail.html"
     context = {"product_detail": product_data}
     return render(request, template_name, context)
 
 class ProductDetail(DetailView):
     model = Product 
-    template_name = 'product_shop/product_detail.html'
-    context_object_name = 'product_detail'
-    pk_url_kwarg = 'pk'
+    template_name = "product_shop/product_detail.html"
+    context_object_name = "product_detail"
+    pk_url_kwarg = "pk"
 
 
 def search_product(request):
     # Get the query 
-    query = request.GET.get('q', '').strip() 
-    category_id = request.GET.get('category', '').strip()
+    query = request.GET.get("q", "").strip() 
+    category_id = request.GET.get("category", "").strip()
     categories = Category.objects.all()   
     results = Product.objects.all()
 
     # Template 
-    template = 'product_shop/search_product.html'
+    template = "product_shop/search_product.html"
     
     # Debugging: print di terminal Django
     print("Query:", query)
@@ -51,9 +61,9 @@ def search_product(request):
         results = results.filter(category__id=category_id)
 
     context = { 
-        'results': results,
-        'query': query,
-        'categories': categories,
+        "results": results,
+        "query": query,
+        "categories": categories,
         }
 
     return render(request, template, context)
@@ -68,14 +78,22 @@ def add_product(request):
     template = "product_shop/add_product.html"
 
     # Add form to add the product 
+    form = AddProductForm()
     if request.method == "POST":
         # Add the form 
-        form = AddProductForm(requets.POST, request.FILES)
+        form = AddProductForm(request.POST, request.FILES)
+        images = request.FILES.getlist("images")
         
         # Check the forms valid or not 
         if form.is_valid():
             product = form.save(commit=False)
             product.seller = request.user
+            product.save()
+
+        return redirect('base:home')
+
+    else:
+        form = AddProductForm()
 
     # Add context for django template
     context = {"add_product_form": form}
