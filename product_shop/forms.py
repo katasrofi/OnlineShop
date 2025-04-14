4380f49905f770d38d1dc67904a94358c2853ab1
katasrofi <danielsensei42@gmail.com>
2025-04-14 14:02:11 +0700

    add Category form

--- product_shop/forms.py
+++ product_shop/forms.py
@@ -1,36 +1,54 @@
-from .models import Product, ProductImage, Category, Review 
+from .models import Product, ProductImage, Category, Review
 from django import forms
+import json
 
-# Create the forms here 
+# Create the forms here
 class AddProductForm(forms.ModelForm):
+    attributes = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3, "placeholder": 'format: {"key": "value"}'}),
+        required=False,
+    )
     class Meta:
-        model = Product 
+        model = Product
         fields = ["category", "name", "description", "attributes", "price", "stock"]
 
-    # Images form to handle several image 
+    # Images form to handle several image
     # images = forms.FileField(widget=forms.FileInput(attrs={"multiple": True}), required=False)
 
-    # Additional information for spesific product 
-    attributes_keys = forms.CharField(required=False, widget=forms.HiddenInput())
-    attributes_values = forms.CharField(required=False, widget=forms.HiddenInput())
+    # Additional information for spesific product
+    # attributes_keys = forms.CharField(required=False, widget=forms.HiddenInput())
+    # attributes_values = forms.CharField(required=False, widget=forms.HiddenInput())
 
-    # Cleaning 
-    def clean(self):
-        cleaned_data = super().clean()
+    # Cleaning
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #
+    #     # Take the key from hidden input
+    #     keys = cleaned_data.get("attributes_keys", "").split(",")
+    #     values = cleaned_data.get("attributes_values", "").split(",")
+    #
+    #     # Create the keys and values dictionary
+    #     attributes = {
+    #         key.strip(): value.strip()
+    #         for key, value in zip(keys, values)
+    #         if key and value
+    #     }
+    #
+    #     # Save the data
+    #     cleaned_data["attributes"] = attributes
+    #     return cleaned_data
 
-        # Take the key from hidden input 
-        keys = cleaned_data.get("attributes_keys", "").split(",")
-        values = cleaned_data.get("attributes_values", "").split(",")
+    def clean_attributes(self):
+        """
+        Make sure the JSON input is valid
+        """
+        data = self.cleaned_data["attributes"]
+        try:
+            return json.loads(data) if data else {}
+        except json.JSONDecodeError:
+            raise forms.ValidationError("JSON format is not valid, use format like this: {\'color\': \'red\'}")
 
-        # Create the keys and values dictionary 
-        attributes = {
-            key.strip(): value.strip()
-            for key, value in zip(keys, values)
-            if key and value 
-        }
-
-        # Save the data 
-        cleaned_data["attributes"] = attributes 
-        return cleaned_data 
-
-
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name", "description"]
