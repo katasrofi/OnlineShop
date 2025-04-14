824240815674659c8a7a090b7409fe293b99f155
katasrofi <danielsensei42@gmail.com>
2025-04-14 14:02:32 +0700

    add url add_category add_product product_detail

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("product_detail/<str:pk>", views.product_detail, name="product_detail"), 
     # path("product_detail/<str:pk>/", views.ProductDetail.as_view(), name="product_detail"),
     path("search_product/", views.search_product, name="search_product"),
-    path("add_product/", views.add_product, name="add_product")
+    path("add_category/", views.add_category, name="add_category"),
+    path("add_product/", views.add_product, name="add_product"),
+    path("product_detail/<str:pk>/edit/", views.edit_product, name="edit_product"),
 ]
