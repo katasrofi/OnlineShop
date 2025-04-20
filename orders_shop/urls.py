f0e0907776c8787ce2aa2361f4a3a0c0a02f9802
katasrofi <danielsensei42@gmail.com>
2025-04-21 01:17:49 +0700

    add process_payment urls

--- orders_shop/urls.py
+++ orders_shop/urls.py
@@ -6,4 +6,5 @@ app_name = "orders_shop"
 
 urlpatterns = [
     path("checkout_order/<str:pk>/", views.checkout_order, name="checkout_order"),
+    path("payment/<str:payment_id>", views.process_payment_view, name="process_payment"),
 ]
