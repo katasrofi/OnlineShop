c2f7096eaf70e244f07b059abcef8224eb3f5bbb
katasrofi <danielsensei42@gmail.com>
2025-04-21 01:20:31 +0700

    organize the look

--- user_shop/models.py
+++ user_shop/models.py
@@ -9,17 +9,24 @@ from django.contrib.auth.models import (
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self,
+                    email,
+                    password=None,
+                    **extra_fields):
         if not email:
             raise ValueError("Email need to fill")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email,
+                          **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self,
+                         email,
+                         password=None,
+                         **extra_fields):
         # Set superuser authority
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -29,7 +36,8 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email,
+                                password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
